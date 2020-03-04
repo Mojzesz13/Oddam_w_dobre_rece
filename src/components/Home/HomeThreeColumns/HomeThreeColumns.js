acd084f546b2f32f1d16c64cb4cfd92098f8710b
Pawel Bozejewski <bozejewski@gmail.com>
2020-03-04 18:25:14 +0100

    3columns

--- src/components/Home/HomeThreeColumns/HomeThreeColumns.js
+++ src/components/Home/HomeThreeColumns/HomeThreeColumns.js
@@ -3,8 +3,22 @@ import './HomeThreeColumns.scss';
 
 const HomeThreeColumns = () => {
     return (
-        <div>
-            3 columny
+        <div className="threeColumnsContainer">
+            <div className="gave">
+                <h2 className="number">10</h2>
+                <h3 className="title">ODDANYCH WORKÓW</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a ligula id diam lacinia elementum et.</p>
+            </div>
+            <div className="organization">
+                <h2 className="number">5</h2>
+                <h3 className="title">WSPARTYCH ORGANIZACJI</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a ligula id diam lacinia elementum et.</p>
+            </div>
+            <div className="collection">
+                <h2 className="number">7</h2>
+                <h3 className="title">ZORGANIZOANYCH ZBIÓREK</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a ligula id diam lacinia elementum et.</p>
+            </div>
         </div>
     );
 };
