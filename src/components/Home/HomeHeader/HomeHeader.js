ef1994b33e7e6eaaac99a4cae1ecbb1196034365
Pawel Bozejewski <bozejewski@gmail.com>
2020-03-04 14:24:30 +0100

    first view

--- src/components/Home/HomeHeader/HomeHeader.js
+++ src/components/Home/HomeHeader/HomeHeader.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import {Link} from 'react-scroll';
+import {NavLink} from "react-router-dom";
 import './HomeHeader.scss'
 import HomeLogin from "./HomeLogin/HomeLogin";
+import titleImg from "../../../assets/Decoration.svg"
 
 const HomeHeader = () => {
     return (
         <div className="headerContainer" id="main">
-            <div className="headerImg">{/*empty*/}</div>
-            <div className="headerText">
-                <div className="headerNavbar">
+            <div className="mainImg">{/*empty*/}</div>
+            <div className="textArea">
+                <div className="navbar">
                     <HomeLogin/>
                     <nav className="headerLinks">
                         <Link
@@ -48,7 +50,18 @@ const HomeHeader = () => {
                         >Kontakt</Link>
                     </nav>
                 </div>
-                <div className="headerTextContent">{/*empty*/}</div>
+                <div className="textContent">
+                    <div className="title">
+                        <h1>Zacznij pomagać!</h1>
+                        <h1>Oddaj niechciane rzeczy w zaufane ręce</h1>
+                    </div>
+                    <div className="titleImg"><img src={titleImg} alt="decoration-bar"/>
+                    </div>
+                    <div className="titleButtons">
+                        <button><NavLink to="/login">ODDAJ RZECZY</NavLink></button>
+                        <button><NavLink to="/login">ZORGANIZUJ ZBIÓRKĘ</NavLink></button>
+                    </div>
+                </div>
             </div>
         </div>
     );
