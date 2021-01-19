e6d874196bef2e3bb866576c2f705d039f65768b
Pawel Bozejewski <bozejewski@gmail.com>
2021-01-19 11:33:40 +0100

    Updates

---
+++ precache-manifest.2fffc7eb81820c7a74a928b225fd2fb0.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3894ab6c293536be8d4058179de66592",
+    "url": "/Oddam_w_dobre_rece/index.html"
+  },
+  {
+    "revision": "4a510a7c35baea0af472",
+    "url": "/Oddam_w_dobre_rece/static/css/main.2a43b338.chunk.css"
+  },
+  {
+    "revision": "dd53528f9aa9677c6b16",
+    "url": "/Oddam_w_dobre_rece/static/js/2.a5528125.chunk.js"
+  },
+  {
+    "revision": "2ef1520116340a0c2930e27b3e7c2120",
+    "url": "/Oddam_w_dobre_rece/static/js/2.a5528125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a510a7c35baea0af472",
+    "url": "/Oddam_w_dobre_rece/static/js/main.a094daa7.chunk.js"
+  },
+  {
+    "revision": "5363f03334bb197e6f9a",
+    "url": "/Oddam_w_dobre_rece/static/js/runtime-main.5fdd483f.js"
+  },
+  {
+    "revision": "4744b9275622ac8af9b8800a739acdb3",
+    "url": "/Oddam_w_dobre_rece/static/media/3 Columns Background@2x.4744b927.png"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Oddam_w_dobre_rece/static/media/Background-Contact-Form.f4db7ddd.jpg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/Oddam_w_dobre_rece/static/media/Background-Form.aab5aa4c.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Oddam_w_dobre_rece/static/media/Decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Oddam_w_dobre_rece/static/media/Facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/Oddam_w_dobre_rece/static/media/Form-Hero-Image.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Oddam_w_dobre_rece/static/media/Home-Hero-Image.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Oddam_w_dobre_rece/static/media/Icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Oddam_w_dobre_rece/static/media/Icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Oddam_w_dobre_rece/static/media/Icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Oddam_w_dobre_rece/static/media/Icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Oddam_w_dobre_rece/static/media/Instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Oddam_w_dobre_rece/static/media/People.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Oddam_w_dobre_rece/static/media/Signature.e54cfcfc.svg"
+  }
+]);
